f1104bf67a8b4a82de5751eb24614d83e7dd2e61
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2024-03-20 21:21:51 -0400

    fix: ajuste na esteira

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { UserModule } from './infrastructure/user/user.module'
 async function bootstrap (): Promise<void> {
   const app = await NestFactory.create(UserModule)
   await app.listen(3000).then(() => {
-    console.log('Server is running on port 3000')
+    console.log('Server is running on port 3000!')
   }).catch((error) => {
     console.error('Error starting server', error)
   })
